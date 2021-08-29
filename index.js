a94b3b73e03d8d169f11e965d71b9dbd00e6b750
jmaloon <joshmaloon@gmail.com>
2021-08-29 14:54:58 -0700

    adding brand logos

---
+++ index.js
@@ -0,0 +1,4 @@
+const abcelleraSection = document.querySelector("#experience_abcellera");
+abcelleraSection.addEventListener('mouseenter', function() {
+    abcelleraSection.querySelector('animate').beginElement();
+})
