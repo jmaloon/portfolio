dc878cd1c9c5afd67cf1522d3d88a466a606da23
jmaloon <joshmaloon@gmail.com>
2021-08-31 00:15:06 -0700

    dark-mode - wip

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const abcelleraSection = document.querySelector("#experience_abcellera");
 abcelleraSection.addEventListener('mouseenter', function() {
     abcelleraSection.querySelector('animate').beginElement();
 })
+
+
+// // add dark class
+// const className = 'dark';
+// const htmlElement = document.querySelector('html');
+
+// // does .dark exist already?
+// htmlElement.classList.contains(className);
+
+// // add .dark
+// htmlElement.classList.add(className);
+// // remove .dark
+// htmlElement.classList.remove(className);
