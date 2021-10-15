92956c745d53199b1538c4c07b6529ea6b5e7042
jmaloon <joshmaloon@gmail.com>
2021-10-14 20:43:50 -0700

    accessibility and disable css

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ abcelleraSection?.addEventListener("mouseenter", function () {
   abcelleraSection.querySelector("animate").beginElement();
 });
 
-const themeButton = document.querySelector("button#dark-mode");
-themeButton?.addEventListener("click", function () {
+const darkModeButton = document.querySelector("button#dark-mode-toggle");
+darkModeButton?.addEventListener("click", function () {
   const darkClassName = "dark";
   const rootElement = document.documentElement;
 
@@ -12,12 +12,28 @@ themeButton?.addEventListener("click", function () {
   if (isDarkModeActive) {
     // if the dark theme is active, we remove the class from the root element
     rootElement.classList.remove(darkClassName);
-    // themeButton.textContent = "🌙";
-    // themeButton.firstChild.textContent = "Dark";
     return;
   }
-
   // otherwise we activate dark mode by adding the class to the root element
   rootElement.classList.add(darkClassName);
-  // themeButton.firstChild.textContent = "Light";
+});
+
+const cssButton = document.querySelector("button#css-toggle");
+cssButton?.addEventListener("click", function () {
+  const stylesheet = document.styleSheets.item(3)
+  const isStyleSheetDisabled = stylesheet.disabled;
+
+  // if the stylesheet is already disabled, no need to confirm action
+  if (isStyleSheetDisabled) {
+    stylesheet.disabled = false;
+    return;
+  }
+  // if the styles are enabled, confirm the action with the user
+  const confirmed = window.confirm(
+    "The page layout will change. Are you sure you want to disable CSS?"
+  );
+
+  if (confirmed) {
+    stylesheet.disabled = true; 
+  }
 });
