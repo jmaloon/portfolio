5690f781160f11f5a48dc5e0489711b4d25fd484
jmaloon <joshmaloon@gmail.com>
2021-10-17 17:33:01 -0700

    inlining font loading

--- font-loading.js
+++
@@ -1,9 +0,0 @@
-if ("fonts" in document) {
-  Promise.all([
-    document.fonts.load("400 16px Quicksand"),
-    document.fonts.load("500 16px Quicksand"),
-    document.fonts.load("700 16px Quicksand"),
-  ]).then(function () {
-    document.documentElement.classList.add("fonts-loaded");
-  });
-}
